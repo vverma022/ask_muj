64512c6ae4bd7c81bb6ffef8693e14acb12c79a8
Vasu Verma <vermavasu069@gmail.com>
2024-08-27 19:42:05 +0530

    Main Page

--- ask_muj/app/page.tsx
+++ ask_muj/app/page.tsx
@@ -1,9 +1,123 @@
+import { Card } from "@/components/ui/card";
+import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { CreatePostCard } from "./components/CreatePostCard";
+import prisma from "./lib/db";
+import { PostCard } from "./components/PostCard";
+import { Suspense } from "react";
+import { SuspenseCard } from "./components/SuspenseCard";
+import Pagination from "./components/Pagination";
+import { unstable_noStore as noStore } from "next/cache";
+import Logo from "@/public/logo.svg";
 
+async function getData(searchParam: string) {
+  noStore();
+  const [count, data] = await prisma.$transaction([
+    prisma.post.count(),
+    prisma.post.findMany({
+      take: 10,
+      skip: searchParam ? (Number(searchParam) - 1) * 10 : 0,
+      select: {
+        title: true,
+        createdAt: true,
+        textContent: true,
+        id: true,
+        imageString: true,
+        Comment: {
+          select: {
+            id: true,
+          },
+        },
+        User: {
+          select: {
+            username: true,
+          },
+        },
+        CommunityName: true,
+        Vote: {
+          select: {
+            userId: true,
+            voteType: true,
+            postId: true,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    }),
+  ]);
 
-export default function Home() {
+  return { data, count };
+}
+
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
-   <div>
-   <h1>Home Page</h1>
-   </div>
+    <div className="max-w-[1000px] mx-auto flex gap-x-10 mt-4 mb-10">
+      <div className="w-[65%] flex flex-col gap-y-5">
+        <CreatePostCard />
+        <Suspense fallback={<SuspenseCard />} key={searchParams.page}>
+          <ShowItems searchParams={searchParams} />
+        </Suspense>
+      </div>
+      <div className="w-[35%]">
+        <Card>
+          <div className="p-2">
+            <div className="flex items-center">
+              <Image src={Logo} alt="Logo" width={40} height={40} />
+              <h1 className="font-medium pl-3">Home</h1>
+            </div>
+            <p className="text-sm text-muted-foreground pt-2">
+              Welcome to MUJ_ASK. Come here to check in with your
+              favorite communites!
+            </p>
+            <Separator className="my-5" />
+
+            <div className="flex flex-col gap-y-3">
+              <Button asChild variant="secondary">
+                <Link href="/r/janmarshal/create">Create Post</Link>
+              </Button>
+              <Button asChild>
+                <Link href="/r/create">Create Community</Link>
+              </Button>
+            </div>
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
+}
+
+async function ShowItems({ searchParams }: { searchParams: { page: string } }) {
+  const { count, data } = await getData(searchParams.page);
+  return (
+    <>
+      {data.map((post) => (
+        <PostCard
+          id={post.id}
+          imageString={post.imageString}
+          jsonContent={post.textContent}
+          subName={post.CommunityName as string}
+          title={post.title}
+          key={post.id}
+          commentAmount={post.Comment.length}
+          userName={post.User?.username as string}
+          voteCount={post.Vote.reduce((acc, vote) => {
+            if (vote.voteType === "UP") return acc + 1;
+            if (vote.voteType === "DOWN") return acc - 1;
+
+            return acc;
+          }, 0)}
+        />
+      ))}
+
+      <Pagination totalPages={Math.ceil(count / 10)} />
+    </>
   );
 }
